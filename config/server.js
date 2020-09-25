d8fcbbc3ac4dba64df7d7bf87515ac31b0cfa75e
frmauro <frmauro8@gmail.com>
2020-09-25 18:45:29 -0300

    create mongoose connection

--- config/server.js
+++ config/server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var consign = require('consign');
 var mongoose = require('mongoose');
+var db = require('./mongodbConnection');
 
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+mongoose.connect(db.uri, { useNewUrlParser: true });
 
 consign()
      .include('routes')
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var app = require('./config/server');
 
-//require('./routes/userRouter')(app);
-
 var port = 8088;
 app.listen(port, function() {
     console.log('O servidor HTTP esta escutando a porta: ' + port);
