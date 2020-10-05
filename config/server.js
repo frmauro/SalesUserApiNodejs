2a1c01f8955b49cebafb0926eed62c10acebfd7f
frmauro <frmauro8@gmail.com>
2020-10-05 17:54:45 -0300

    implementation cors configuration

--- config/server.js
+++ config/server.js
@@ -1,32 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var consign = require('consign');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
+app.options('*', cors());
+
+app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+  });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-
-// Add headers
-app.use(function (req, res, next) {
-
-     // Website you wish to allow to connect
-     res.setHeader('Access-Control-Allow-Origin', '*');
- 
-     // Request methods you wish to allow
-     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
- 
-     // Request headers you wish to allow
-     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
- 
-     // Set to true if you need the website to include cookies in the requests sent
-     // to the API (e.g. in case you use sessions)
-     res.setHeader('Access-Control-Allow-Credentials', true);
- 
-     // Pass to next layer of middleware
-     next();
- });
-
 
 consign()
      .include('/routes')
