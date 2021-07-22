c4f7e0240cf338ace37758e2911dde1973b84198
frmauro <frmauro8@gmail.com>
2021-07-22 16:26:51 -0300

    implementation grpc server

--- config/server.js
+++ config/server.js
@@ -4,44 +4,9 @@ var consign = require('consign');
 var cors = require('cors');
 
 
-
-
-// GRPC *******************************************************************
-
-const PROTO_PATH = "./user.proto";
-var grpc = require('grpc');
-var protoLoader = require("@grpc/proto-loader");
-
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-     keepCase: true,
-     longs: String,
-     enums: String,
-     arrays: true
- });
-
-const user =  { id: '1', name: 'Francisco Mauro', password: '123', token: '', userType: 'administrator', status: 'Active'  };
-  
-var userProto = grpc.loadPackageDefinition(packageDefinition);
-const server = new grpc.Server();
-
-server.addService(userProto.UserService.service, {
-     findByEmailAndPassword: (_, callback) => {
-         callback(null, user)
-     },
- })
-
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
-console.log('Server running at http://0.0.0.0:50051')
-server.start()
-
- // GRPC *******************************************************************
-
-
-
-
-
 const swaggerUi = require('swagger-ui-express'),
  swaggerDocument = require('../swagger.json');
+
 
 var app = express();
 app.use(cors());
@@ -58,6 +23,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 consign()
      .include('/routes')
--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 var app = require('./config/server');
 
+
+// GRPC *******************************************************************
+
+const PROTO_PATH = "./user.proto";
+var grpc = require('grpc');
+var protoLoader = require("@grpc/proto-loader");
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+     keepCase: true,
+     longs: String,
+     enums: String,
+     arrays: true
+ });
+
+const user =  { id: '1', name: 'Francisco Mauro', password: '123', token: 'hjsdf8sd7fsdfshjksdfs87986786', userType: 'administrator', status: 'Active'  };
+  
+var userProto = grpc.loadPackageDefinition(packageDefinition);
+const server = new grpc.Server();
+
+server.addService(userProto.SalesUserApi.UserService.service, {
+     findByEmailAndPassword: (_, callback) => {
+         callback(null, user)
+     },
+ })
+
+server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+ // GRPC *******************************************************************
+
+
+
 var port = 8088;
 app.listen(port, function() {
-    console.log('O servidor HTTP esta escutando a porta: ' + port);
+        console.log('Server GRPC - running at http://0.0.0.0:50051')
+        server.start()
+        console.log('O servidor HTTP esta escutando a porta: ' + port);
 });
 
 
