4ca0af9d81ed29eef9e88e9422fc6b54e7f840ee
frmauro <frmauro8@gmail.com>
2021-07-23 12:59:42 -0300

    user service grpc with mongoose

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = require('./config/server');
 const PROTO_PATH = "./user.proto";
 var grpc = require('grpc');
 var protoLoader = require("@grpc/proto-loader");
+const user = require('./models/user');
 
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
      keepCase: true,
@@ -14,14 +15,34 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
      arrays: true
  });
 
-const user =  { id: '1', name: 'Francisco Mauro', password: '123', token: 'hjsdf8sd7fsdfshjksdfs87986786', userType: 'administrator', status: 'Active'  };
   
 var userProto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
 
 server.addService(userProto.SalesUserApi.UserService.service, {
      findByEmailAndPassword: (_, callback) => {
-         callback(null, user)
+
+                 var db = app.models.user;
+                 var users = db.Mongoose.model('users', db.UserSchema, 'users');
+                 var user =  { id: '1', name: 'Teste', password: '999', token: 'hjsdf8sd7fsdfshjksdfs87986786', userType: 'administrator', status: 'Active'  };
+            
+                users.find({}, function(err, result) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(result);
+                        //console.log(result[0].name);
+                         user.id = result[0].id;
+                         user.name = result[0].name;
+                         user.password = result[0].password;
+                         user.token = result[0].token;
+                         user.userType = result[0].userType;
+                         user.status = result[0].status;
+                        //res.json(result);
+                    }
+                    callback(null, user)
+                });
+
      },
  })
 
